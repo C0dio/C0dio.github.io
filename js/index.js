007229e2d2bb4bc67821a67894030a332244dce9
C0dio <68840768+C0dio@users.noreply.github.com>
2022-04-20 11:59:26 +0100

    CAMRA website
    Copied the CAMRA website project into a git repository for GitHub pages

---
+++ js/index.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  /* static inheritence */
+  // load the following static sections on each page
+  $(function(){
+      $("#header-placeholder").load("/templates/header.html");
+      $("#sidebar-placeholder").load("/templates/sidebar.html");
+      $("#footer-placeholder").load("/templates/footer.html");
+    });
+
+  /* general */
+  // handle contact form submission
+  $("#contact-btn").click(function(){
+    if ( !$("#contact-form")[0].checkValidity() )
+      return;
+    // create a mailto email and populate the fields
+    window.location = 'mailto:' + $("#contact-selector").val() + '?subject=' + $("#contact-subject").val() + '&body=' + $("#contact-message").val();
+  });
+});
